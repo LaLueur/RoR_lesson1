e7b8394e25e60490a21f850149ad1cb9a25f8c39
Yuliya Karpenko <LaLueur@users.noreply.github.com>
2014-10-19 03:16:57 +0300

    Create id3.rb

---
+++ id3.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What is your patronymic?"
+patronymic = gets.chomp
+puts "What is your surname?"
+surname = gets.chomp
+puts "Hi! We are glad to see you, #{name} #{patronymic} #{surname}!"
